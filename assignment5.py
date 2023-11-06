1b47f5cb25bd65eb0b919105806bec70dad81aea
JohnnyTamale <foxsauceplanet@protonmail.com>
2023-11-05 18:19:13 -0800

    Function sorts to check if userr input in list

--- assignment5.py
+++ assignment5.py
@@ -18,22 +18,47 @@ for i in range(100): #This range will dictate how long the list will be. It will
     empty_list.append(rand_range) #append the values to the empty list
     new_random_list = empty_list # reassign the empty list to a variable that makes sense.
 
-# print(new_random_list)#Test print random list in terminal
+print(new_random_list)#Test print random list in terminal
 
 #Create a labels
 #user_label will prompt the user for an input.
 user_label = tk.Label(root, text="Enter an integer and see if its in our randomized list")
-user_label.pack()
+user_label.pack(pady=0)
+
+list_label = tk.Label(root, text ="")
+list_label.pack()
+
+#Textbox for user input
+textbox = tk.Entry(root)
+textbox.pack(pady=10)
+
+# this loop is to test that an element is in the random list
+# for i in new_random_list:
+# 	if(i == 17):
+# 		print("Element Exists")
 
 # This label for the list needs to appear after the button is clicked. This function will take the new list add it as text to a separate label
 def display_list():
-    list_label = tk.Label(root, text = new_random_list)
-    list_label.pack(pady=10)
+    user_input = int(textbox.get())
+    print(f"User Input: {user_input}")
+    print(f"new_random_list: {new_random_list}")
+    
+    if user_input in new_random_list:
+        list_label.config(text=f"Your number {user_input} is in the list")
+    else:
+        list_label.config(text=f"Your number {user_input} is not in the list")
+
+            
+    
+    # if:
+    #     list_label = tk.Label(root, text = "This number is not in the list")
+    #     list_label.pack()
+
 
 # Create buttons
 # button1 will use the display_list function and show the new random list on a  label below the user label
 button1 = tk.Button(root, text="Display List", command=display_list)
-button1.pack()
+button1.pack(pady=30)
 
 ######MISSING CODE#######
 ## I NEED A TEXT BOX FOR USER INPUT
